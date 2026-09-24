f454f251bcfc4d0d757be9feec52beb696a1246c
Bekfest3 <abhi123nakarmi@gmail.com>
2026-09-24 19:19:11 +0545

    Integers in C

---
+++ c/Datatypes/int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("Integer datatype is outputted using %d\n");
+
+    //Example
+    int a = 10,b=20;
+    printf("%d\n",a);
+    printf("%d\n",b);
+    printf("%d",a+b);
+    return 0;
+}
