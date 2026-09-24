f17ec6454ea1e5be4de13c9529368aef22667e9c
Bekfest3 <abhi123nakarmi@gmail.com>
2026-09-24 19:00:07 +0545

    Input + Output

--- c/standard_io.c
+++ c/standard_io.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(){
-    printf("First time using C");
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    printf("Sum of %d and %d is %d\n",a,b,a+b);
     return 0;
 }
