6a0501b69defb6fd6dc22b1bfff02a5202f93503
Bekfest3 <abhi123nakarmi@gmail.com>
2026-09-24 19:21:33 +0545

    Float in C

---
+++ c/Datatypes/float.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("Float datatype is outputted using %f\n");
+
+    //Example
+    float a = 10.5,b=20.5;
+    printf("%f\n",a);
+    printf("%f\n",b);
+    printf("%f",a+b);
+    return 0;
+}
